7d654d3b130353ffa3a39c7bfa09772952bb8752
agustinsaavedra2 <agustin.saavedra@usach.cl>
2023-10-03 20:55:43 -0300

    Funciones selectora de TDA System

--- TDASystem_20677670_SaavedraOlmos.rkt
+++ TDASystem_20677670_SaavedraOlmos.rkt
@@ -16,3 +16,41 @@
   (lambda (name InitialChatbotCodeLink . chatbot)
     (cons name (list InitialChatbotCodeLink
                (eliminar-duplicados3 (eliminar-concurrencias3 chatbot)) null null))))
+
+; Nombre: name-system
+; Dominio: system.
+; Recorrido: name.
+; Descripción: Corresponde a una función selectora de TDA System. Su entrada recibe
+; un sistema. Su salida es el nombre del sistema entrante.
+
+(define name-system
+  (lambda (system)
+    (car system)))
+
+; Nombre: InitialChatbotCodeLink-system
+; Dominio: system.
+; Recorrido: InitialChatbotCodeLink.
+; Descripción: Corresponde a una función selectora de TDA System. Su entrada recibe
+; un sistema. Su salida es el código inicial del chatbot del sistema entrante.
+
+(define InitialChatbotCodeLink-system
+  (lambda (system)
+    (cadr system)))
+
+; Nombre: chatbots-system
+; Dominio: system.
+; Recorrido: chatbots.
+; Descripción: Corresponde a una función selectora de TDA System. Su entrada recibe
+; un sistema. Su salida corresponde a los chatbots del sistema entrante.
+
+(define chatbots-system
+  (lambda (system)
+    (caddr system)))
+
+(define user-system
+  (lambda (system)
+    (cadddr system)))
+
+(define user-login
+  (lambda (system)
+    (cadddr (cdr system))))
